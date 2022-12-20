080cfd8c4621f878676cbd068072d3c1aed90dda
matt190589 <112311267+matt190589@users.noreply.github.com>
2022-12-20 15:36:56 +0000

    dependencies installed

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations Genius
+        Congratulations Genius. Always remember to Npm I!
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉
